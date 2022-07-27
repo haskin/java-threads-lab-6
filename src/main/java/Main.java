b8f4986a5c2053677dac78ca824b7d4f3261a3d9
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-27 12:07:48 -0400

    finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
     }
 }
@@ -21,8 +24,16 @@ class PrimeLogger implements Runnable {
         this.num = num;
     }
 
+    boolean isPrime(int possiblePrime) {
+        if (possiblePrime == 0 || possiblePrime == 1)
+            return false;
+        return !IntStream.rangeClosed(2, possiblePrime / 2).anyMatch(number -> possiblePrime % number == 0);
+    }
+
     @Override
     public void run() {
         // print num if it is prime
+        if (isPrime(num))
+            System.out.println("This number was prime: " + num);
     }
 }
